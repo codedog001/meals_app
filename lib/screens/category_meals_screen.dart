767401f190b0e7a3702228304f2f138bdcd3656c
Mohit Jain <mohitjain9855@gmail.com>
2021-01-14 23:33:09 +0530

    First Push

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryID;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryID, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: categoryMeals[index].id,
+            title: categoryMeals[index].title,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
